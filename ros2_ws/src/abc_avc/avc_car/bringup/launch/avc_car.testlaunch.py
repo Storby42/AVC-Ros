1f66515edb24ad8b17fb3899a3a76ddf9909c7a5
cs0826 <claireshen2008@gmail.com>
2026-04-06 15:58:04 -0400

    Update avc_car.testlaunch.py

--- ros2_ws/src/abc_avc/avc_car/bringup/launch/avc_car.testlaunch.py
+++ ros2_ws/src/abc_avc/avc_car/bringup/launch/avc_car.testlaunch.py
@@ -175,6 +175,16 @@ def generate_launch_description():
         # the velocity comands are sent to the bicycle controller, which then send commands to the hardware interface
     )
 
+    robot_localization = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('robot_localization'), 'launch', 'ekf.launch.py')]),
+    )
+    navigation = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('robot_description'), 'launch', 'avc_navigation.launch.py')]),
+                    launch_arguments={'params_file': os.path.join(get_package_share_directory('robot_description'), 'config', 'nav2_params.yaml')}.items()
+    )
+
     nodes = [
         twist_stamper,
         twist_mux,
@@ -184,6 +194,7 @@ def generate_launch_description():
         delay_rviz_after_joint_state_broadcaster_spawner,
         delay_robot_controller_spawner_after_joint_state_broadcaster_spawner,
         robot_pose_broadcaster_spawner,
+        navigation,
     ]
 
     return LaunchDescription(declared_arguments + nodes)
