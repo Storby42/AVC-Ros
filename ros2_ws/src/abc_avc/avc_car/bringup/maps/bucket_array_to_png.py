605852aa56c1bc597a265b8aee2fc4bf950788fe
Bean <70710230+Storby42@users.noreply.github.com>
2026-04-13 19:06:14 -0400

    test stuff

--- ros2_ws/src/abc_avc/avc_car/bringup/maps/bucket_array_to_png.py
+++ ros2_ws/src/abc_avc/avc_car/bringup/maps/bucket_array_to_png.py
@@ -11,8 +11,10 @@ map_conversion=20*feet_to_meters
 
 # starts at (0,0) bottom left with frame of width 100, height 80
 # indexes through buckets from bottom right -> clockwise
-buckets=np.array([[80,20], [50,25],[50,35],[20,20],[20,60],[50,57.5],[50,62.5],[80,60]])
-ramp=[16,40]
+#buckets=np.array([[80,20], [50,25],[50,35],[20,20],[20,60],[50,57.5],[50,62.5],[80,60]])
+buckets=np.array([[35,40], [35,20],[20,40]])
+#ramp=[16,40]
+ramp=[100,100]
 
 background = (255, 255, 255)
 black = (0, 0, 0)
@@ -28,7 +30,7 @@ for bucket in buckets:
 ramp_draw=pg.Rect(ramp[0]*map_conversion,ramp[1]*map_conversion,4*map_conversion,4*map_conversion)
 pg.draw.rect(win, black, ramp_draw, width)
 
-fname = "map.png"
+fname = "maptest.png"
 win = pg.transform.flip(win,False,True) # surface, flip_x, flip_y - note that this only changes the file, not the display
 pg.image.save(win, fname)
 print("file {} has been saved".format(fname))
