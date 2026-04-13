c3571671994a2b32a765db4645485511e7f328bb
Annabeth-pan <annabethpan7@gmail.com>
2026-04-13 18:09:42 -0400

    higher covariances
    ekf trusts it a little too much

--- ros2_ws/src/buckalization/buckalization/buckalization.py
+++ ros2_ws/src/buckalization/buckalization/buckalization.py
@@ -212,12 +212,12 @@ class BuckalizationNode(Node):
         # (x, y, z, rotation about X axis, rotation about Y axis, rotation about Z axis)
         # FIND THE VARIANCES OF X Y YAW. Assumed 0 for z, pitch, roll, and covariances
         msg.pose.covariance = [
-            0.05, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.05, 0.0, 0.0, 0.0, 0.0,
+            0.3, 0.0, 0.0, 0.0, 0.0, 0.0,
+            0.0, 0.3, 0.0, 0.0, 0.0, 0.0,
             0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
             0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
             0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 1.0
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.5
             ]
         self.publisher_buckalization.publish(msg)
     
